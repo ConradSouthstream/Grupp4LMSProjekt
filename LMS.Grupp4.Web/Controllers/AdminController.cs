e0479809b0eb79b1d433b4f31b946f8109dcb298
Mats Antonsson <matan848@hotmail.com>
2021-04-29 13:04:00 +0200

    Add Admin Update and Delete user

--- LMS.Grupp4.Web/Controllers/AdminController.cs
+++ LMS.Grupp4.Web/Controllers/AdminController.cs
@@ -13,12 +13,14 @@ namespace LMS.Grupp4.Web.Controllers
     public class AdminController : Controller
     {
         private readonly UserManager<Anvandare> userManager;
+        private readonly IPasswordHasher<Anvandare> passwordHasher;
 
-        public AdminController(UserManager<Anvandare> userManager)
+        public AdminController(UserManager<Anvandare> userManager, IPasswordHasher<Anvandare> passwordHasher)
         {
             this.userManager = userManager;
+            this.passwordHasher = passwordHasher;
         }
-
+      
         public IActionResult Index()
         {
             return View(userManager.Users);
@@ -50,6 +52,68 @@ namespace LMS.Grupp4.Web.Controllers
                 }
             }
             return View(user);
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                    user.Email = email;
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = passwordHasher.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("Index", userManager.Users);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
         }
     }
 }
