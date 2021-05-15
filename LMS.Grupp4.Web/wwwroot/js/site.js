9c6d3cefa85b69fdac40357415d52419f709ec14
Oscar Andersson <oscarandersson2@gmail.com>
2021-05-15 22:42:46 +0200

    Implementering av sortering av litteratur

--- LMS.Grupp4.Web/wwwroot/js/site.js
+++ LMS.Grupp4.Web/wwwroot/js/site.js
@@ -1,3 +1,13 @@
 ﻿$(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+// Anropas när man vill sortera en kolumn i listan av litteratur
+function SortBy(sort) {
+
+    //alert('Test: ' + sort);
+
+    document.getElementById('SortBy').value = sort;  
+    document.SearchLitteraturForm.submit();
+}
